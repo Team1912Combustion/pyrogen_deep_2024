07cd46fd755dd73459f9d05249a22a6d23eccf06
clarkrowley <c_rowley@bellsouth.net>
2024-11-28 13:53:58 -0600

    Update PPPyroTest

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/EstimTest.java
@@ -0,0 +1,58 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.commands.DefaultDrive;
+import org.firstinspires.ftc.teamcode.pyrolib.ftclib.command.CommandOpMode;
+import org.firstinspires.ftc.teamcode.pyrolib.ftclib.command.RunCommand;
+import org.firstinspires.ftc.teamcode.pyrolib.ftclib.command.InstantCommand;
+import org.firstinspires.ftc.teamcode.pyrolib.ftclib.gamepad.GamepadEx;
+import org.firstinspires.ftc.teamcode.pyrolib.ftclib.gamepad.GamepadKeys;
+import org.firstinspires.ftc.teamcode.subsystems.Drive;
+import org.firstinspires.ftc.teamcode.subsystems.Arm;
+import org.firstinspires.ftc.teamcode.subsystems.Elevator;
+import org.firstinspires.ftc.teamcode.subsystems.Intake;
+import org.firstinspires.ftc.teamcode.commands.*;
+
+import org.firstinspires.ftc.teamcode.subsystems.Estimator;
+import org.firstinspires.ftc.teamcode.subsystems.Odometry;
+import org.firstinspires.ftc.teamcode.subsystems.Vision;
+
+@TeleOp
+public class EstimTest extends CommandOpMode {
+
+    private Drive m_drive;
+    private DefaultDrive m_driveCommand;
+    private Vision m_vision;
+    private Estimator m_estimator;
+    private Odometry m_odometry;
+    private GamepadEx m_driverStick;
+
+    public void initialize() {
+
+        m_driverStick = new GamepadEx(gamepad1);
+
+        m_vision = new Vision(hardwareMap, telemetry);
+        m_odometry = new Odometry(hardwareMap);
+        m_estimator = new Estimator(m_odometry, m_vision);
+
+        // create our drive object
+        m_drive = new Drive(hardwareMap);
+        register(m_drive);
+        m_driveCommand = new DefaultDrive(m_drive,
+                () -> m_driverStick.getLeftX(),
+                () -> m_driverStick.getLeftY(),
+                () -> m_driverStick.getRightX(),
+                () -> m_estimator.getRotation());
+        m_drive.setDefaultCommand(m_driveCommand);
+
+        telemetry.addLine(String.format("Estimator XYZ %6.1f %6.1f %6.1f  (inch)",
+                m_estimator.getX(),
+                m_estimator.getY(),
+                m_estimator.getPose().getHeading()));
+
+        // update telemetry every loop
+        schedule(new RunCommand(telemetry::update));
+        run();
+    }
+}
