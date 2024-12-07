60bd7444543cfae37c32bdc101ff4d4cf78059c9
Clark Rowley <clarkdavidrowley@gmail.com>
2024-12-07 17:06:24 -0600

    Add P Controllers for Arm and Elevator.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/ElevatorDown.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.commands;
+
+import org.firstinspires.ftc.teamcode.pyrolib.ftclib.command.CommandBase;
+import org.firstinspires.ftc.teamcode.robot.Constants;
+import org.firstinspires.ftc.teamcode.subsystems.Elevator;
+
+/**
+ * A command to run the elevator back in.
+ */
+public class ElevatorUp extends CommandBase {
+
+    private final Elevator m_elevator;
+    private double target;
+
+    public ElevatorUp(Elevator elevator) {
+        m_elevator = elevator;
+        addRequirements(m_elevator);
+    }
+
+    @Override
+    public void execute() {
+        m_elevator.runToPosition(
+                m_elevator.get_position() +
+                Constants.ElevatorConstants.threshold);
+    }
+
+}
